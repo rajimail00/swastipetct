d173507a924685a5019c3f033a29ee76e5f876c5
Raji S <mailtogeorgest@gmail.com>
2024-01-17 16:06:24 +0530

    SEO tags

---
+++ schema.js
@@ -0,0 +1,19 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "name": "Swasti PET/CT Imaging & Nuclear Medicine",
+      "url": "https://www.swastipetct.com/",
+      "logo": "https://www.swastipetct.com/assets/images/favicon.jpeg",
+      "contactPoint": [
+        {
+          "@type": "ContactPoint",
+          "telephone": "+91 85899 89945",
+          "contactType": "customer service",
+          "email": "swastipetct@gmail.com",
+          "areaServed": "IN",
+          "availableLanguage": "en"
+        }
+      ]
+    }
+    </script>
